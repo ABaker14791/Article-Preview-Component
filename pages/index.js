bc57f6526c5607e6cbfb94fbd8d254d2ebc2e61d
Adam Baker <abakermtb@gmail.com>
2022-06-13 23:11:05 +0100

    finished styling mobile design

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import Styles from "../styles/Home.module.scss";
 import Image from "next/image";
+// images
 import drawers from "../images/drawers.jpg";
+import avatar from "../images/avatar-michelle.jpg";
+// icons
+import shareIcon from "../images/icon-share.svg";
 
 export default function Home() {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div>
       <Head>
@@ -36,12 +43,35 @@ export default function Home() {
               it felt slightly bare and uninviting. I&apos;ve got some simple
               tips to help you make any room feel complete.
             </p>
-            <div className={Styles.CardAuthor}>
-              {/* <image /> */}
-              <h2 className={Styles.CardName}>Michelle Appleton</h2>
-              <p className={Styles.CardDate}>28 Jun 2020</p>
-              {/* <button>Share</button> */}
-            </div>
+            {clicked ? (
+              <div className={Styles.CardFooter}>
+                <div className={Styles.CardAuthor}>
+                  <h2 className={Styles.CardName}>Share</h2>
+                </div>
+
+                <button className={Styles.ShareBtn} onClick={setClicked}>
+                  <Image src={shareIcon} />
+                </button>
+              </div>
+            ) : (
+              <div className={Styles.CardFooter}>
+                <div className={Styles.AuthorContainer}>
+                  <Image
+                    src={avatar}
+                    className={Styles.Avatar}
+                    width="40"
+                    height="40"
+                  />
+                  <div className={Styles.CardAuthor}>
+                    <h2 className={Styles.CardName}>Michelle Appleton</h2>
+                    <p className={Styles.CardDate}>28 Jun 2020</p>
+                  </div>
+                </div>
+                <button className={Styles.ShareBtn} onClick={setClicked}>
+                  <Image src={shareIcon} />
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </main>
