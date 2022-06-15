139a47a32724b7527a8c0f94ef24a3a4877ad5ef
Adam Baker <adam_baker@live.co.uk>
2022-06-15 21:43:35 +0100

    desktop responsive

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,6 @@ export default function Home() {
       <Head>
         <title>Frontend Mentor | Article preview component</title>
         <meta name="description" content="Article preview component" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
         <link
           rel="icon"
           type="image/png"
@@ -35,50 +29,52 @@ export default function Home() {
 
       <main className={Styles.CardContainer}>
         <div className={Styles.Card}>
-          <Image src={drawers} />
-          <div className={Styles.CardContent}>
-            <h1 className={Styles.CardHeading}>
-              Shift the overall look and feel by adding these wonderful touches
-              to furniture in your home
-            </h1>
-            <p className={Styles.CardText}>
-              Ever been in a room and felt like something was missing? Perhaps
-              it felt slightly bare and uninviting. I&apos;ve got some simple
-              tips to help you make any room feel complete.
-            </p>
-          </div>
-          {clicked ? (
-            <div className={Styles.CardFooterActive}>
-              <div className={Styles.SocialIcons}>
-                <p className={Styles.ShareText}>Share</p>
+          <Image src={drawers} className={Styles.Heroimg} />
+          <div className={Styles.CardRight}>
+            <div className={Styles.CardContent}>
+              <h1 className={Styles.CardHeading}>
+                Shift the overall look and feel by adding these wonderful
+                touches to furniture in your home
+              </h1>
+              <p className={Styles.CardText}>
+                Ever been in a room and felt like something was missing? Perhaps
+                it felt slightly bare and uninviting. I&apos;ve got some simple
+                tips to help you make any room feel complete.
+              </p>
+            </div>
+            {clicked ? (
+              <div className={Styles.CardFooterActive}>
+                <div className={Styles.SocialIcons}>
+                  <p className={Styles.ShareText}>Share</p>
 
-                <Image src={facebookIcon} />
-                <Image src={twitterIcon} />
-                <Image src={pinterestIcon} />
-              </div>
-              <button className={Styles.ShareBtn} onClick={setClicked}>
-                <Image src={shareIcon} />
-              </button>
-            </div>
-          ) : (
-            <div className={Styles.CardFooter}>
-              <div className={Styles.AuthorContainer}>
-                <Image
-                  src={avatar}
-                  className={Styles.Avatar}
-                  width="40"
-                  height="40"
-                />
-                <div className={Styles.CardAuthor}>
-                  <h2 className={Styles.CardName}>Michelle Appleton</h2>
-                  <p className={Styles.CardDate}>28 Jun 2020</p>
+                  <Image src={facebookIcon} />
+                  <Image src={twitterIcon} />
+                  <Image src={pinterestIcon} />
                 </div>
+                <button className={Styles.ShareBtn} onClick={setClicked}>
+                  <Image src={shareIcon} />
+                </button>
               </div>
-              <button className={Styles.ShareBtn} onClick={setClicked}>
-                <Image src={shareIcon} />
-              </button>
-            </div>
-          )}
+            ) : (
+              <div className={Styles.CardFooter}>
+                <div className={Styles.AuthorContainer}>
+                  <Image
+                    src={avatar}
+                    className={Styles.Avatar}
+                    width="40"
+                    height="40"
+                  />
+                  <div className={Styles.CardAuthor}>
+                    <h2 className={Styles.CardName}>Michelle Appleton</h2>
+                    <p className={Styles.CardDate}>28 Jun 2020</p>
+                  </div>
+                </div>
+                <button className={Styles.ShareBtn} onClick={setClicked}>
+                  <Image src={shareIcon} />
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </main>
     </div>
